a9091f0bb54ba537ba2226271787a6ae16a9ae2d
Divya2306 <96518035+Divya2306@users.noreply.github.com>
2023-01-23 20:00:32 +0530

    remove routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" exact to="/">{props.title}</Link>
+          <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" eaxct to="/about">{props.aboutText}</Link>
-              </li>
+              {/* <li className="nav-item">
+                <a className="nav-link" href="#">{props.aboutText}</a>
+              </li> */}
             </ul>
           </div>
           <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'} mx-2`}>
